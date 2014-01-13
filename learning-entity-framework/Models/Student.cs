1e4d1be63168e2e70f0932737e50322f01aec9a0
Payam Yavari <payam@yavari.se>
2014-01-13 16:36:53 +0100

    Finished part 4
    http://www.asp.net/mvc/tutorials/getting-started-with-ef-5-using-mvc-4/creating-a-more-complex-data-model-for-an-asp-net-mvc-application

--- learning-entity-framework/Models/Student.cs
+++ learning-entity-framework/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,24 @@ namespace learning_entity_framework.Models
 {
     public class Student
     {
-        public int StudentID{get;set;}
+        public int StudentID { get; set; }
+
+        [StringLength(50, MinimumLength = 1)]
         public string LastName { get; set; }
+
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
