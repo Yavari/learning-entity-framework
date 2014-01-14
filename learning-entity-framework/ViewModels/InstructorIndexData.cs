702bbb114cbb890b0aefc6c9de7e83ab19c549e3
Payam Yavari <payam@yavari.se>
2014-01-14 14:16:45 +0100

    Finished part 5
    http://www.asp.net/mvc/tutorials/getting-started-with-ef-5-using-mvc-4/reading-related-data-with-the-entity-framework-in-an-asp-net-mvc-application

---
+++ learning-entity-framework/ViewModels/InstructorIndexData.cs
@@ -0,0 +1,15 @@
+﻿using learning_entity_framework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace learning_entity_framework.ViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
